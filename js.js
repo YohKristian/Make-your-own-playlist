b72c39d9173fc4abe6c893dd496bfcd7328ff089
khalidmi <khalidibrahim2508@gmail.com>
2022-06-23 16:45:37 +0700

    add database to js

---
+++ js.js
@@ -0,0 +1,39 @@
+let data = [
+    {
+        judul : 'Save Your Tears',
+        artis : 'The Weeknd',
+        genre : 'R&B',
+        link : 'https://www.youtube.com/watch?v=XXYlFuWEuKI'
+    },
+    {
+        judul : `Don't Matter`,
+        artis : 'Akon',
+        genre : 'R&B',
+        link : `https://www.youtube.com/watch?v=JWA5hJl4Dv0`
+    },
+    {
+        judul : `Satru`,
+        artis : 'Denny Caknan',
+        genre : 'Koplo',
+        link : `https://www.youtube.com/watch?v=fKVgm3_x-OI`
+    },
+    {
+        judul : `Lemah Teles`,
+        artis : 'Happy Asmara',
+        genre : 'Koplo',
+        link : `https://www.youtube.com/watch?v=3tmwbTeQWmk`
+    },
+    {
+        judul : `Don't Look Back in Anger`,
+        artis : 'Oasis',
+        genre : 'Rock',
+        link : `https://www.youtube.com/watch?v=fKVgm3_x-OI`
+    },
+    {
+        judul : `I Miss You`,
+        artis : 'Blink-182',
+        genre : 'Rock',
+        link : `https://www.youtube.com/watch?v=s1tAYmMjLdY`
+    }
+  ]
+
